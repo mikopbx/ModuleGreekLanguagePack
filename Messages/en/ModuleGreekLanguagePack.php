b2c189e7a93b9fcabc16cf6964139dc7ba3fd932
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:04 +0700

    Initial commit: ModuleGreekLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleGreekLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleGreekLanguagePack' => 'Greek Language Pack',
+    'SubHeaderModuleGreekLanguagePack' => 'Complete Greek language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleGreekLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleGreekLanguagePack' => 'Языковой пакет - Греческий',
+    'SubHeaderModuleGreekLanguagePack' => 'Комплексная поддержка греческого языка для системы MikoPBX',
+];
